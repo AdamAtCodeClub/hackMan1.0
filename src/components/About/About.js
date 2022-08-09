2f356c4061b4931bc4ebe9df303298fc0bb938de
Adam Drummond <adam@Adams-MacBook-Pro.local>
2022-08-09 13:45:05 +0100

    added faq

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,6 +15,22 @@ export default function About(){
                     <h2>What is hackMan?</h2>
                     <p>hackMan is the Isle of Man’s first ever hackathon, held between the 28th to the 30th of October. We are looking to host upwards of 1OO “hackers” (competitors), not only from across the Isle of Man but also further afield, with an additional support pathway for those travelling to the island.</p>
                 </Col>
+                <Col>
+                    <Row>
+                        <Col sm={12} md={6}>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8787.294640685568!2d-4.411613909833771!3d54.32747103244227!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48639f3e6c7e2977%3A0xa41cf10fc8ade59f!2sMountain%20View%20Innovation%20Centre!5e0!3m2!1sen!2sim!4v1659875062194!5m2!1sen!2sim" width="600" height="450"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </Col>
+                        <Col sm={12} md={6}>
+                            <h2>Where are we?</h2>
+                            <p>We will be taking residency in Studio One at <a href="https://mvic.im">Mountain View Innovation Centre</a> in the Isle of Man.</p>
+                            <p>Mountain View Innovation Centre
+                                <br />Jurby Road
+                                <br />Lezayre, Ramsey
+                                <br />Isle of Man
+                                <br />IM7 2DZ</p>
+                        </Col>
+                    </Row>
+                </Col>
             </Row>
         </Container>
     )
--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,48 +1,68 @@
 import './FAQ.css'; 
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function FAQ(){
-
 
     return(
         <Container className="FAQ" id="faq">
             <h1>Frequently Asked Questions</h1>
-            {/* <Row className="faqContent">
+            <Row className="faqContent">
+                
                 <Col sm={12} md={6} lg={4} className="qa">
-                    <div className="qabg">
-                    <p className="question">Question?</p>
-                    <p className="answer">Answer.</p></div>
+                    <div className="qaInner">
+                        <p className="question">Who can attend hackMan?</p>
+                        <p className="answer"><b>Anyone can attend hackMan!</b><br />Whilst the event is aimed at students and recent graduates, if you can get yourself to the Isle of Man, you're welcome to join in!</p>
+                    </div>
                 </Col>
                 <Col sm={12} md={6} lg={4} className="qa">
-                    <p className="question">Question?</p>
-                    <p className="answer">Answer.</p>
+                    <div className="qaInner">
+                        <p className="question">Do I have to be studying Computer Science or Computing?</p>
+                        <p className="answer"><b>Nope!</b><br />As mentioned before, anyone is welcome at hackMan, as long as you have a passion to create something, you're good to come.</p>
+                    </div>
                 </Col>
                 <Col sm={12} md={6} lg={4} className="qa">
-                    <p className="question">Question?</p>
-                    <p className="answer">Answer.</p>
+                    <div className="qaInner">
+                        <p className="question">Can under 18s take part in hackMan?</p>
+                        <p className="answer"><b>Yes - kind of.</b><br />We recommend only locals as under 18s will need to source their own accommodation overnight, such as going home.</p>
+                    </div>
                 </Col>
                 <Col sm={12} md={6} lg={4} className="qa">
-                    <p className="question">Question?</p>
-                    <p className="answer">Answer.</p>
+                    <div className="qaInner">
+                        <p className="question">Do I need a team before joining?</p>
+                        <p className="answer"><b>It's up to you.</b><br />If you don't have a team, there will be opportunities to meet new people when travelling to the event or during check-in. The maximum team size is 4 hackers. Alternatively, you can take part individually.</p>
+                    </div>
                 </Col>
                 <Col sm={12} md={6} lg={4} className="qa">
-                    <p className="question">Question?</p>
-                    <p className="answer">Answer.</p>
+                    <div className="qaInner">
+                        <p className="question">Can I start before arrival?</p>
+                        <p className="answer"><b>Absolutely not!</b><br />To make hackMan as fair and enjoyable as possible for all, hacking must not start until after the opening ceremony. You are, of course, welcome to come up with ideas and plans beforehand.</p>
+                    </div>
                 </Col>
                 <Col sm={12} md={6} lg={4} className="qa">
-                    <p className="question">Question?</p>
-                    <p className="answer">Answer.</p>
+                    <div className="qaInner">
+                        <p className="question">Can I join online?</p>
+                        <p className="answer"><b>Not this time.</b><br />In order to create the best experience for all attendees, we wanted to keep it all in person so we could focus fully on their experience.</p>
+                    </div>
                 </Col>
                 <Col sm={12} md={6} lg={4} className="qa">
-                    <p className="question">Question?</p>
-                    <p className="answer">Answer.</p>
+                   <div className="qaInner">
+                        <p className="question">How much does hackMan cost?</p>
+                        <p className="answer"><b>Completely Nothing!</b><br />We aim to provide an all-inclusive weekend for hackers. From the moment you step foot on the Island (<i>and possibly before!</i>), meals, snacks and much more will all be provided.</p>
+                    </div>
                 </Col>
                 <Col sm={12} md={6} lg={4} className="qa">
-                    <p className="question">Question?</p>
-                    <p className="answer">Answer.</p>
+                    <div className="qaInner">
+                        <p className="question">What do I need to bring?</p>
+                        <p className="answer"><b>The essentials.</b><br />You will, of course, need a computer to code on. We recommend bringing something comfortable to sleep with.</p>
+                    </div>
                 </Col>
-            </Row> */}
-            <h2><i>Coming Soon...</i></h2>
+                <Col sm={12} md={6} lg={4} className="qa">
+                    <div className="qaInner">
+                        <p className="question">Is there a Code of Conduct?</p>
+                        <p className="answer"><b>Yes.</b><br />We follow the <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf">Major League Hacking Code of Conduct</a>. Put short, be respectful.</p>
+                    </div>
+                </Col>
+            </Row>
         </Container>
     )
 
